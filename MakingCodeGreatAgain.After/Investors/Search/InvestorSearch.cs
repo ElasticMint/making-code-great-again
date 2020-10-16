5c9b9235da07ab9f61bf58048b1bde2e49f68249
Andy Garner <andy.garner@elasticmint.com>
2020-10-16 14:15:54 +0100

    Log ES query

--- MakingCodeGreatAgain.After/Investors/Search/InvestorSearch.cs
+++ MakingCodeGreatAgain.After/Investors/Search/InvestorSearch.cs
@@ -4,38 +4,44 @@ using Elasticsearch.Net;
 using MakingCodeGreatAgain.After.ElasticSearch;
 using MakingCodeGreatAgain.After.ElasticSearch.Investors.Model;
 using MakingCodeGreatAgain.After.ElasticSearch.Investors.Query;
+using MakingCodeGreatAgain.After.Logging;
 using Nest;
+using Serilog;
 
 namespace MakingCodeGreatAgain.After.Investors.Search
 {
     public class InvestorSearch : IInvestorSearch
     {
         private readonly IElasticClient _elasticClient;
+        private readonly ILogger _logger;
 
         public InvestorSearch(IElasticClient elasticClient)
         {
             _elasticClient = elasticClient;
+            _logger = Log.ForContext<InvestorSearch>();
         }
 
         public async Task<List<ViewModel>> Get(Request request)
         {
-            var result = await _elasticClient.SearchAsync<Investor>(
-                q => q.Index(Indexes.Investors)
-                    .SearchType(SearchType.DfsQueryThenFetch)
-                    .Type("")
-                    .From((request.Page -1) * 20)
-                    .Size(20)
-                    .Source(s => s.Includes(i => i.Fields(
-                        "id",
-                        "name",
-                        "address.country"
-                        )))
-                    .ScriptFields(script => script.InvestmentAmountUsd().InvestmentAmountUsd())
-                    .Query(query => query.IsActive() && 
-                                    query.IsInCountry(request.Countries))
-                    .Sort(s => s.Field(f => f.Field(request.SortBy).Order(request.SortOrder)))
-                );
+            var query = new SearchDescriptor<Investor>()
+                .Index(Indexes.Investors)
+                .SearchType(SearchType.DfsQueryThenFetch)
+                .Type("")
+                .From(request.From)
+                .Size(request.PageSize)
+                .Source(s => s.Includes(i => i.Fields(
+                    "id",
+                    "name",
+                    "address.country"
+                )))
+                .ScriptFields(script => script.InvestmentAmountUsd().InvestmentAmountUsd())
+                .Query(q => q.IsActive() &&
+                            q.IsInCountry(request.Countries))
+                .Sort(s => s.Field(f => f.Field(request.SortBy).Order(request.SortOrder)));
 
+            _logger.LogQuery("Search for investors", "GET investors/search", query);
+
+            var result = await _elasticClient.SearchAsync<Investor>(query);
             return Map(result);
         }
 
--- MakingCodeGreatAgain.After/Investors/Search/Request.cs
+++ MakingCodeGreatAgain.After/Investors/Search/Request.cs
@@ -8,6 +8,9 @@ namespace MakingCodeGreatAgain.After.Investors.Search
         public string[] Sectors { get; set; }
         public string SortBy { get; set; }
         public SortOrder SortOrder { get; set; }
-        public int Page { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+
+        public int From => (Page - 1) * PageSize;
     }
 }
